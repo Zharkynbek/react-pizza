b08f2b3899f5e19b941cdb3fea97e184d80a2943
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-08-16 22:34:23 +0200

    create navigation and burger menu

---
+++ src/components/Nav/RightNav.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nawrap;
+
+  li {
+    padding: 18px 10px;
+  }
+
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color: #0d2538;
+    position: fixed;
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 300px;
+    padding-top: 3.5rem;
+    transition: transform 0.3s ease-in-out;
+
+    li {
+      color: #fff;
+    }
+  }
+`;
+
+const RightNav = ({ open }) => {
+  return (
+    <Ul open={open}>
+      <li>Home</li>
+      <li>Pizzas</li>
+      <li>Döner</li>
+      <li>Salate</li>
+      <li>Getränke</li>
+      <li>Contact us</li>
+    </Ul>
+  );
+};
+
+export default RightNav;
